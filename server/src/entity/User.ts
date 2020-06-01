ed9a9d7ca932feea4dc7a3ede8ea8b5852a70f25
Artyom Moskalew <mosarvit@Artyoms-MBP.fritz.box>
2020-06-01 14:56:30 +0200

    AUget all users

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,11 +1,14 @@
+import { Field, Int, ObjectType } from "type-graphql";
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+@ObjectType()
 @Entity('users')
 export class User extends BaseEntity {
-
+    @Field(()=> Int)
     @PrimaryGeneratedColumn()
     id: number;
 
+    @Field()
     @Column()
     email: string;
 
