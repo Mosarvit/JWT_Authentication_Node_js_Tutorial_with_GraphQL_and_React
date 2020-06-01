a6dfe485a190847e7f77efaa2ff34f4efef4c37e
Artyom Moskalew <mosarvit@Artyoms-MBP.fritz.box>
2020-06-01 14:50:38 +0200

    AUregister endpoint dded

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import { ApolloServer } from "apollo-server-express";
 import express from "express";
 import "reflect-metadata";
 import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
 import { UserResolver } from "./UserResolver";
 
 
@@ -9,6 +10,8 @@ import { UserResolver } from "./UserResolver";
 (async()=>{
     const app = express();
     app.get('/', (_req, res)=>res.send("Say Hello"))
+
+    await createConnection()
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
@@ -27,20 +30,3 @@ import { UserResolver } from "./UserResolver";
 
 })()
 
-// createConnection().then(async connection => {
-
-//     console.log("Inserting a new user into the database...");
-//     const user = new User();
-//     user.firstName = "Timber";
-//     user.lastName = "Saw";
-//     user.age = 25;
-//     await connection.manager.save(user);
-//     console.log("Saved a new user with id: " + user.id);
-
-//     console.log("Loading users from the database...");
-//     const users = await connection.manager.find(User);
-//     console.log("Loaded users: ", users);
-
-//     console.log("Here you can setup and run express/koa/any other framework.");
-
-// }).catch(error => console.log(error));
