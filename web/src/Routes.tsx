e3b5839970a5b88ca56603b748771688aea6b9d3
Artyom Moskalew <mosarvit@Artyoms-MBP.fritz.box>
2020-06-02 16:59:17 +0200

    routing done

---
+++ web/src/Routes.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+
+export const Routes = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <header>
+          <div>
+            <div>
+              <Link to="/">home</Link>
+            </div>
+            <div>
+              <Link to="/register">register</Link>
+            </div>
+            <div>
+              <Link to="/login">login</Link>
+            </div>
+          </div>
+        </header>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
